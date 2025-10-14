27e16c5dc80dad2bd86481d408a301de1b9ad6ed
LuminaryCLoud88 <michaellevesque288@gmail.com>
2025-10-14 14:22:40 -0700

    Removed public dir and added docs

--- src/main.py
+++ src/main.py
@@ -2,13 +2,19 @@ from textnode import TextNode, TextType
 import os, shutil
 from copy_static import copyfilesrecursive
 from gencontent import generate_pages_recursive
+import sys
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 def main():
+   basepath = default_basepath
+   if len(sys.argv) > 1:
+       basepath = sys.argv[1]
+
    print("Deleting public directory...")
    if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -17,7 +23,7 @@ def main():
    copyfilesrecursive(dir_path_static, dir_path_public)
   
    print("Generating content...")
-   generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+   generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 if __name__ == "__main__":
